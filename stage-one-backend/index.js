cb0a5bfed143096d71d7d01410f00d73df569056
3akare <bakaredavid007@gmail.com>
2024-06-30 21:58:10 +0100

    rough work

--- stage-one-backend/index.js
+++ stage-one-backend/index.js
@@ -1,13 +1,34 @@
 const express = require("express")
-const app = express();
+require('dotenv').config();
 
-const PORT = 8080;
+const app = express();
+const PORT = process.env.PORT;
+const MODE = process.env.MODE;
+
+app.use(express.json());
+
+// app.use((err, req, res, next)=>{
+//     res.redirect("/home")
+// })
 
 app.get("/home", (req, res) => {
-    const ipAddress = req.headers['x-forwarded-for'];
-    return res.json({ ipAddress: typeof(ipAddress), ipAddress, status: "Ok" })
+    let ipAddresses;
+
+    // console.log(MODE)
+    if (MODE == "dev"){
+        ipAddresses = "197.211.59.15, 10.203.33.163, 10.204.46.38";
+    }
+    else{
+        ipAddresses = req.headers['x-forwarded-for'];
+    }
+
+    // There is a list of IP addresses because the hosting provider is likely to use load balcncer and shit like that
+   
+    const ipAddress = ipAddresses.split(", ").length > 0 ? ipAddresses.split(", ")[0] : ipAddresses;
+    // return res.json({ ipAddress: typeof(ipAddress), ipAddress, status: "Ok" })
+    return res.json({ ok: ipAddress });
 })
 
-app.listen(8080, () => {
-    console.log("App is live")
+app.listen(PORT, () => {
+    console.log("Server is listening on" + PORT)
 })
