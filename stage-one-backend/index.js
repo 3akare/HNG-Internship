0830f08b029db35aec64c7fa25ac2dcff5c7eacd
David Bakare <bakaredavid007@gmail.com>
2024-07-01 16:34:17 +0100

    feat(index.js): default URL

--- stage-one-backend/index.js
+++ stage-one-backend/index.js
@@ -4,6 +4,10 @@ require("dotenv").config();
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).json({ welcome: "HNG Internship" });
+});
+
 app.get("/api/hello", async (req, res) => {
   try {
     const name = req.query.visitor_name || "Stranger";
