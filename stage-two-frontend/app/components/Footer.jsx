b64345dbaed0788ef00514f82f74fdb5438a8d56
David Bakare <bakaredavid007@gmail.com>
2024-07-05 11:41:47 +0100

    feat(Footer.jsx): Add Footer component

---
+++ stage-two-frontend/app/components/Footer.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import logo from "../../app/assets/white-logo.svg";
+
+const Footer = () => {
+  return (
+    <footer className="w-full bg-black h-96">
+      <div className="container mx-auto max-w-7xl w-full">
+        <div></div>
+        <div></div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
