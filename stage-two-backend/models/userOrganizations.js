f4c542c302ea406a1beed5dadb61f304bc0adbf6
3akare <bakaredavid007@gmail.com>
2024-07-05 21:33:27 +0100

    feat(model): dataase models/entities

---
+++ stage-two-backend/models/userOrganizations.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) => {
+    const userOrganization = sequelize.define('userOrganization', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        orgId: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        userId: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
+    return userOrganization;
+}
